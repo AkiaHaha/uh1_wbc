dec62908a23feb7979ee3a9a712de2eb27d167c4
AkiaHaha <daniel666cqu@163.com>
2024-11-04 14:17:07 +0800

    TSV; Params  that could control wrist EE' s Xyz;

--- src/unitree_wbc/example/src/robotController.cpp
+++ src/unitree_wbc/example/src/robotController.cpp
@@ -320,6 +320,22 @@ bool RobotController::motionPlan(){// @Daniel240523
     xyzDotFootTgt[1] = Eigen::Vector3d::Zero();
     rpyDotFootTgt[1] = Eigen::Vector3d::Zero();
 
+    // // LeftArm
+    // rpyArmTgt[0] = rpyArmInit[0];
+    // xyzArmTgt[0] = xyzArmInit[0];
+                   
+    // rpyDotArmTgt[0] = Eigen::Vector3d::Zero();
+    // xyzDotArmTgt[0] = xyzDotArmInit[0];
+                    
+
+    // // RightArm
+    // rpyArmTgt[1] = rpyArmInit[1];
+    // xyzArmTgt[1] = xyzArmInit[1];
+
+    // rpyDotArmTgt[1] = Eigen::Vector3d::Zero();
+    // xyzDotArmTgt[1] = xyzDotArmInit[1];
+            
+
     // LeftArm
     rpyArmTgt[0] = rpyArmInit[0];
     xyzArmTgt[0] << xyzArmInit[0].x()+configParams.armForward*mPlanSinUpDown,
