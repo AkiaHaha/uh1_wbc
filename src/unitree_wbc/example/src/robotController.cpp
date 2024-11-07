452a3b09cb1dd546464c0f8e40d09d133c53a694
AkiaHaha <daniel666cqu@163.com>
2024-11-08 05:36:49 +0800

    TSV; Add a new wbt file with pendulum;

--- src/unitree_wbc/example/src/robotController.cpp
+++ src/unitree_wbc/example/src/robotController.cpp
@@ -98,10 +98,10 @@ bool RobotController::getValuePosCurrent(Eigen::VectorXd &jntPosCur){
 bool RobotController::update(double timeCtrlSys, webotsState& robotStateSim){
     time += DT;
     stateEstimation(robotStateSim);
-    // motionPlan1();
-    // motionPlan2();
+    motionPlan1();
+    motionPlan2();
     // motionPlan3();
-    motionPlan4();
+    // motionPlan4();
     taskControl();
     return true;
 }
